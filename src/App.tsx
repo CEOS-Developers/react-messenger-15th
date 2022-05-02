9f750f8670ebfe7b96b1a156b507088b6f4f7956
juhyojeong <juhyojeong58@gmail.com>
2022-05-02 10:32:24 +0900

    style: 컨테이너 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-  return <div>15기 파이팅</div>;
-}
+import styled from "styled-components";
+
+const App = () => {
+  return (
+    <Wrapper>
+      <Container></Container>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Container = styled.div`
+  height: 640px;
+  width: 360px;
+  border: 1px solid lightgrey;
+  border-radius: 7px;
+`;
 
 export default App;
