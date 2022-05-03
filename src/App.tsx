1a7a6b9df75f9b5f47f9729691653137a92203aa
24siefil <24siefil@gmail.com>
2022-05-03 23:15:43 +0900

    backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
+import styled from 'styled-components';
 import { ChatRoomHeader } from './components/chatroom/ChatRoomHeader';
 import { ChatRoomMain } from './components/chatroom/ChatRoomMain';
 import { ChatRoomForm } from './components/chatroom/ChatRoomForm';
 
-function App() {
+export function App() {
   return (
     <>
-      <ChatRoomHeader />
-      <ChatRoomMain />
-      <ChatRoomForm />
+      <Screen>
+        <ChatRoomHeader />
+        <ChatRoomMain />
+        <ChatRoomForm />
+      </Screen>
     </>
   );
 }
 
-export default App;
+const Screen = styled.div`
+  width: 360px;
+  height: 600px;
+  margin-top: 80px;
+
+  display: flex;
+  flex-direction: column;
+
+  border-radius: 20px;
+  box-shadow: 0 0 25px rgba(105, 85, 85, 0.5);
+`;
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+${normalize}
+
+@import url(https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap);
+
+* {
+    box-sizing: border-box;
+    outline: none;
+  }
+
+html,
+body {
+    margin: 0;
+    padding: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-family: 'Noto Sans KR', 'sans-serif', 'serif';
+}
+`;
