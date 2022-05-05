ed4cb3c15b94bef8baf63e9e6d3fff564bd457aa
한규진 <david0218@naver.com>
2022-05-05 13:49:28 +0900

    feat : 스크롤 최하단으로 가기 버튼

--- src/components/chatroom/RoomBody.tsx
+++ src/components/chatroom/RoomBody.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 import { ChatType } from '../../Interface';
 import ChatItem from './ChatItem';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 const RoomBody = ({ chats }: { chats: ChatType[] }) => {
+    const [scrollButtonVisible, setScrollButtonVisible] = useState<boolean>(false);
+    const [scrollBarVisible, setScrollBarVisible] = useState<boolean>(false);
     const bodyRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
@@ -11,11 +13,36 @@ const RoomBody = ({ chats }: { chats: ChatType[] }) => {
         }
     }, [chats]);
 
+    const handleScrollY = () => {
+        if (bodyRef.current) {
+            const roomBodyScrollY = bodyRef.current.scrollHeight - bodyRef.current.scrollTop;
+            if (roomBodyScrollY > 1000) {
+                console.log('visible');
+                setScrollButtonVisible(true);
+            } else {
+                console.log('hide');
+                setScrollButtonVisible(false);
+            }
+        }
+
+        setScrollBarVisible(true);
+        setTimeout(() => {
+            setScrollBarVisible(false);
+        }, 500);
+    };
+
+    const goToBottom = () => {
+        if (bodyRef.current) {
+            bodyRef.current.scrollTo(0, bodyRef.current.scrollHeight);
+        }
+    };
+
     return (
-        <RoomBodyContainer ref={bodyRef}>
+        <RoomBodyContainer ref={bodyRef} onScroll={handleScrollY}>
             {chats.map((chat: ChatType) => (
                 <ChatItem chat={chat} key={chat.chatId} />
             ))}
+            {scrollButtonVisible ? <GoToBottom onClick={goToBottom} /> : null}
         </RoomBodyContainer>
     );
 };
@@ -27,4 +54,17 @@ const RoomBodyContainer = styled.div`
     overflow-y: scroll;
     background-color: #8989bb;
     background-image: linear-gradient(315deg, #8989bb 0%, #a5a4cb 74%);
+    position: relative;
+`;
+
+const GoToBottom = styled.button`
+    position: sticky;
+    bottom: 30px;
+    float: right;
+
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    border: none;
+    box-shadow: rgba(0, 0, 0, 0.05) 0 0 10px;
 `;
