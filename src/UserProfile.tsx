3351e5103f498a0b62963e9d0e954e165de785f4
YoommY <seeun0792@gmail.com>
2022-05-06 17:24:52 +0900

    feat : user data, image 추가

--- src/UserProfile.tsx
+++ src/UserProfile.tsx
@@ -2,6 +2,8 @@ import {Profileprops} from "./type";
 import React from "react";
 import { Dispatch, SetStateAction, useCallback} from "react";
 import {Profileimg,Profile,ProfileName,ProfileIntro} from "./UserProfileDesign"
+import nowUser from "./data/user.json";
+import ProfileImage from "./Image";
 
 function UserProfile({changeUser,setChangeUser} : Profileprops){
 
