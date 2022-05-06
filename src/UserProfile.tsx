4bd3d1201c608ecf9ccac2ad8037e17cec068a96
YoommY <seeun0792@gmail.com>
2022-05-06 17:19:20 +0900

    feat: UserProfile 컴포넌트 만들기

---
+++ src/UserProfile.tsx
@@ -0,0 +1,25 @@
+import {Profileprops} from "./type";
+import React from "react";
+import { Dispatch, SetStateAction, useCallback} from "react";
+import {Profileimg,Profile,ProfileName,ProfileIntro} from "./UserProfileDesign"
+
+function UserProfile({changeUser,setChangeUser} : Profileprops){
+
+     const toggleUser = useCallback (() => {
+         changeUser ? setChangeUser(0) : setChangeUser(1);
+        },[]);
+    //확장성 고려해서 roomid로 바꾸기
+
+    const {name, intro} = nowUser[changeUser];
+    
+    return( 
+    <Profile>
+        <Profileimg  src = {ProfileImage[changeUser]} onClick = {toggleUser}/>
+        <ProfileName>{name}<span> 🟢</span></ProfileName>
+        <ProfileIntro>{intro}</ProfileIntro>
+    </Profile>
+    )
+
+    };
+
+ export default UserProfile;
---
+++ src/type.tsx
@@ -0,0 +1,7 @@
+import { Dispatch, SetStateAction} from "react";
+
+export interface Profileprops{
+    changeUser : number;
+    setChangeUser : Dispatch<SetStateAction<number>>
+};
+
