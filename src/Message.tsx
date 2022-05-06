ee9cbe181fc605ac670090819e4905cc3436210f
YoommY <seeun0792@gmail.com>
2022-05-06 20:23:11 +0900

    feat : MessageDesign 컴포넌트 작성, data 불러오기

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,9 @@
 import { MessageItem } from "./type";
 import {useRef,useEffect,useMemo} from "react";
+import {Wrapper,ChatMessage,ChatProfile,Chatcontainer,MessageBox,ChatName} from "./MessageDesign";
+import ProfileImage from "./Image"; 
+import user from "./data/user.json";
+import React from "react";
 
 type MessageList = {
     messageList : MessageItem[];
@@ -10,12 +14,32 @@ function Message({messageList} : MessageList){
     const messageRef = useRef<HTMLDivElement>(null);
 
     const ScrollEvent = useEffect(()=>{
-         if(messageRef.current)
-         messageRef.current.scrollTo({
+        if(messageRef.current)
+        messageRef.current.scrollTo({
             top: messageRef.current.scrollHeight,
             behavior: "smooth",
-          });
+        })
     },[messageList]); //메세지리스트가 변할때만 스크롤에 대한 렌더링
+
+
+    return(
+  
+        <Wrapper ref={messageRef}>
+            {messageList.map((message) => (
+                <Chatcontainer key ={message.userId}>
+                <ChatProfile src = {ProfileImage[message.userId]}/>
+                 <MessageBox>
+                     <ChatName>{user[message.userId].name} 님의 말: </ChatName>
+                      <ChatMessage>{message.text}</ChatMessage>   
+                 </MessageBox>
+            </Chatcontainer>
+                ))
+            }
+            
+        </Wrapper>
+        
+ 
+    );
 
 }
 
