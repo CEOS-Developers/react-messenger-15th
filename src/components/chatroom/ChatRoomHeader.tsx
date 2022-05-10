abb74c8a1692d2b0390eea6485103d3fc14029d8
Minji Oh <ohminji@ewhain.net>
2022-05-10 13:04:46 +0900

    Chore: Re-order files

---
+++ src/components/chatroom/ChatRoomHeader.tsx
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+import { FiChevronLeft } from 'react-icons/fi';
+import { IChatRoomHeaderProps } from '../Interfaces';
+
+const ChatRoomHeader = ({
+  currentUserId,
+  setCurrentUserId,
+}: IChatRoomHeaderProps) => {
+  const handleCurrentUserToggle = () => {
+    currentUserId === 'user1'
+      ? setCurrentUserId('user0')
+      : setCurrentUserId('user1');
+  };
+
+  return (
+    <Header>
+      <Button>
+        <FiChevronLeft />
+      </Button>
+
+      <ProfileWrapper onClick={handleCurrentUserToggle}>
+        <ProfileImg
+          src={`${process.env.PUBLIC_URL}/imgs/${currentUserId}.jpg`}
+        />
+        <TextWrapper>
+          <UserName>{currentUserId}</UserName>
+          <Typing>Typing...</Typing>
+        </TextWrapper>
+      </ProfileWrapper>
+      <Button />
+    </Header>
+  );
+};
+
+const Header = styled.header`
+  padding: 5%;
+  display: flex;
+  justify-content: space-evenly;
+
+  border-bottom: 1px solid #e2e2e2;
+`;
+
+const Button = styled.div`
+  width: 10%;
+  height: 100%;
+  margin-top: 6%;
+  margin-right: 12%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProfileWrapper = styled.section`
+  width: 40%;
+  height: 75%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const ProfileImg = styled.img`
+  width: 40px;
+  height: 40px;
+
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+const TextWrapper = styled.div`
+  width: 56%;
+  height: 75%;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const UserName = styled.div`
+  width: 100%;
+  height: 50%;
+  font-size: 16px;
+
+  display: flex;
+  align-items: center;
+`;
+
+const Typing = styled.div`
+  width: 100%;
+  height: 50%;
+
+  font-size: 12px;
+  color: #c8c8c8;
+`;
+
+export default ChatRoomHeader;
---
+++ src/pages/ChatRoomScreen.tsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import InputForm from '../components/chatroom/InputForm';
+import ChatRoomBody from '../components/chatroom/ChatRoomBody';
+import ChatRoomHeader from '../components/chatroom/ChatRoomHeader';
+import { IChatRoomScreenProps } from '../components/Interfaces';
+
+const ChatRoomScreen = ({
+  chatList,
+  currentUserId,
+  setCurrentUserId,
+  setChatList,
+}: IChatRoomScreenProps) => {
+  return (
+    <Wrapper>
+      <ChatRoomHeader
+        currentUserId={currentUserId}
+        setCurrentUserId={setCurrentUserId}
+      />
+
+      <ChatRoomBody chatList={chatList} />
+
+      <InputForm
+        currentUserId={currentUserId}
+        setCurrentUserId={setCurrentUserId}
+        chatList={chatList}
+        setChatList={setChatList}
+      />
+    </Wrapper>
+  );
+};
+
+export default ChatRoomScreen;
+
+const Wrapper = styled.section`
+  width: 350px;
+  height: 700px;
+
+  border: 1px solid #e2e2e2;
+
+  @media screen and (min-width: 768px) {
+    margin-top: 10%;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 100vw;
+    height: calc(var(--vh, 1vh) * 100);
+  }
+`;
