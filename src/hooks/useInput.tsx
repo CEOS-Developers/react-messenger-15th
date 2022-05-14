98cc50d9fc7d9a35b9465f6f69fc2370d93c246d
chaaerim <cofla411@ewhain.net>
2022-05-15 03:41:20 +0900

    refactor : apply useCallback to handleInputChange

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,15 +1,20 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 const useInput = (initialInput: string) => {
   const [textinput, setTextInput] = useState(initialInput);
 
   //input창에 입력된 todo를 todoText로 설정
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const {
-      target: { value },
-    } = e;
-    setTextInput(value);
-  };
+  const handleInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>): void => {
+      {
+        const {
+          target: { value },
+        } = e;
+        setTextInput(value);
+      }
+    },
+    []
+  );
 
   // input창 값 비우기
   const handleInputInitialize = () => {
